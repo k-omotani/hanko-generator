5bd67c3f99129c183b07c345ca88358625465b99
k-omotani <k.omotani.0225@gmail.com>
2022-08-12 19:56:27 +0900

    test slider component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Preview from './component/Preview';
+
+import Slider from '@mui/material/Slider';
+import Boy from '@mui/icons-material/Boy';
+import Stack from '@mui/material/Stack';
+import AirlineSeatReclineExtraIcon from '@mui/icons-material/AirlineSeatReclineExtra';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Preview />
+        <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+          <AirlineSeatReclineExtraIcon />
+          <Slider
+            aria-label="Small steps"
+            defaultValue={0.00000005}
+            step={1.0}
+            marks
+            min={0}
+            max={90}
+            valueLabelDisplay="auto"
+          />
+          <Boy />
+        </Stack>
       </header>
     </div>
   );
---
+++ src/component/Preview.tsx
@@ -0,0 +1,13 @@
+
+function Preview() {
+    return (
+        <div className="preview">
+            <header className="preview-header">
+                Preview
+
+            </header>
+        </div>
+    );
+}
+
+export default Preview;
