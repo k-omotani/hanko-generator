8d87e091a913c08d2a05a266c9dfac62dde47389
k-omotani <k.omotani.0225@gmail.com>
2022-12-09 23:27:47 +0900

    layout base

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,71 @@
 import React from 'react';
-import './App.css';
 import Preview from './component/Preview';
 
-import Slider from '@mui/material/Slider';
-import Boy from '@mui/icons-material/Boy';
-import Stack from '@mui/material/Stack';
-import AirlineSeatReclineExtraIcon from '@mui/icons-material/AirlineSeatReclineExtra';
+import { AppBar, Button, Card, CardActions, CardContent, Container, CssBaseline, GlobalStyles, Grid, Paper, Toolbar, Typography } from '@mui/material';
+import { Box } from '@mui/system';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Preview />
-        <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
-          <AirlineSeatReclineExtraIcon />
-          <Slider
-            aria-label="Small steps"
-            defaultValue={0.00000005}
-            step={1.0}
-            marks
-            min={0}
-            max={90}
-            valueLabelDisplay="auto"
-          />
-          <Boy />
-        </Stack>
-      </header>
-    </div>
+    <React.Fragment>
+      <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
+      <AppBar
+        position="static"
+        color="default"
+        elevation={0}
+        sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+      >
+        <Toolbar sx={{ flexWrap: 'wrap' }}>
+          <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+            Hanko Generator
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container disableGutters maxWidth="sm" component="main" sx={{ pt: 8, pb: 6 }}>
+        <Typography
+          component="h1"
+          variant="h4"
+          align="center"
+          color="text.primary"
+          gutterBottom
+        >
+          はんこ風絵文字ジェネレータ
+        </Typography>
+        <Typography variant="h5" align="center" color="text.secondary" component="p">
+          Japanese Traditional CampanyのDXのため、古き良きはんこ文化を残すため、我らは立ち上がる。
+        </Typography>
+      </Container>
+      <Container maxWidth="md" component="main">
+        <Grid container spacing={2} alignItems="flex-end">
+          <Grid xs={12} md={4}>
+            <Card>{card}</Card>
+          </Grid>
+          <Grid xs={12} md={4}>
+            <Card>{card}</Card>
+          </Grid>
+          <Grid xs={12} md={4}>
+            <Card>{card}</Card>
+          </Grid>
+        </Grid>
+      </Container>
+    </React.Fragment>
   );
 }
 
+const card = (
+  <CardContent>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'baseline',
+        mb: 2,
+      }}>
+      <Typography component="h2" variant="h3" color="text.primary">
+        プレビュー
+      </Typography>
+    </Box>
+  </CardContent>
+);
+
 export default App;
+
--- src/component/Preview.tsx
+++ src/component/Preview.tsx
@@ -1,12 +1,27 @@
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
 
 function Preview() {
     return (
-        <div className="preview">
-            <header className="preview-header">
-                Preview
-
-            </header>
-        </div>
+        <Card>
+            <CardMedia
+                component="img"
+                alt="green iguana"
+                image=""
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    米 プレビュー
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                    species, ranging across all continents except Antarctica
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+            </CardActions>
+        </Card>
     );
 }
 
