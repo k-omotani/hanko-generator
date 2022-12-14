d5cf4d4ce144a63f88caa1be65d825763ea3d821
k-omotani <k.omotani.0225@gmail.com>
2022-12-14 19:09:32 +0900

    はんこを表示する

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { AppBar, Card, CardContent, Container, GlobalStyles, Grid, Toolbar, Typography } from '@mui/material';
+import { AppBar, Card, CardContent, Container, GlobalStyles, Grid, Slider, TextField, Toolbar, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 
+export const cropCanvas = (text: string, angle: number): HTMLCanvasElement => {
+  const target = document.createElement("canvas");
+  const ctx = target.getContext("2d")!;
+  const fontSize = '40px'
+
+  var x = (target.width / 2);
+  var y = (target.height / 2);
+  ctx.beginPath();
+  ctx.arc(x, y, 50, 0, Math.PI * 2, true); // Outer circle
+  ctx.strokeStyle = "red"
+  ctx.lineWidth = 2
+  ctx.stroke();
+
+
+  ctx.beginPath();
+
+  ctx.translate(x, y);
+  ctx.rotate(angle * Math.PI / 180);
+  ctx.translate(-x, -y);
+  ctx.font = fontSize + ' serif';
+  ctx.fillStyle = 'red';
+  ctx.textAlign = 'center';
+
+  ctx.fillText(text, x, y + 15);
+
+  return target;
+};
+
 function App() {
+  const [name, setName] = useState("山田");
+  const [angle, setAngle] = useState(0);
+  console.log(angle)
   return (
     <React.Fragment>
       <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
@@ -34,15 +65,25 @@ function App() {
         </Typography>
       </Container>
       <Container maxWidth="md" component="main">
-        <Grid container spacing={2} alignItems="flex-end">
+        <Grid container spacing={2}>
           <Grid xs={12} md={4}>
-            <Card>{card}</Card>
+            <Card>{
+              input(
+                (n: string) => {
+                  setName(n)
+                },
+                (a: number) => {
+                  setAngle(a)
+                }
+              )}</Card>
           </Grid>
-          <Grid xs={12} md={4}>
-            <Card>{card}</Card>
-          </Grid>
-          <Grid xs={12} md={4}>
-            <Card>{card}</Card>
+          <Grid container xs={12} md={8}>
+            <Grid md={12}>
+              <Card>{card("面谷", 20)}</Card>
+            </Grid>
+            <Grid md={12}>
+              <Card>{card(name, angle)}</Card>
+            </Grid>
           </Grid>
         </Grid>
       </Container>
@@ -50,7 +91,7 @@ function App() {
   );
 }
 
-const card = (
+const card = (text: string, angle: number) => (
   <CardContent>
     <Box
       sx={{
@@ -59,9 +100,41 @@ const card = (
         alignItems: 'baseline',
         mb: 2,
       }}>
-      <Typography component="h2" variant="h3" color="text.primary">
-        プレビュー
-      </Typography>
+      <img src={cropCanvas(text, angle).toDataURL()}></img>
+    </Box>
+  </CardContent>
+);
+
+const input = (
+  onChangeName: (name: string) => void,
+  onChangeAngle: (angle: number) => void,
+) => (
+  <CardContent>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'baseline',
+        mb: 2,
+      }}>
+      <TextField
+        label="山田"
+        defaultValue={"山田"}
+        multiline
+        maxRows={8}
+        minRows={4}
+        onChange={(event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => { onChangeName(event.target.value) }}
+      />
+      <Slider
+        defaultValue={50}
+        aria-label="Default"
+        valueLabelDisplay="auto"
+        onChange={(event: React.SyntheticEvent | Event, value: number | Array<number>) => {
+          if (typeof value === "number") {
+            onChangeAngle(value)
+          }
+        }}
+      />
     </Box>
   </CardContent>
 );
